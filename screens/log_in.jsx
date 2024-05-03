08c709b8fe9f10980d7a102a1882862216519924
JkInfosysGloble <85567438+G-ourav@users.noreply.github.com>
2024-05-03 19:11:26 +0530

    Create log_in.jsx

---
+++ screens/log_in.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Log_in = () => {
+  return (
+    <View>
+      <Text>Log_in</Text>
+    </View>
+  );
+};
+
+export default Log_in;
