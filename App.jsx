bc1bc4e0725805b75450385620cdbd1fef25677d
JkInfosysGloble <85567438+G-ourav@users.noreply.github.com>
2024-05-04 17:04:48 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,40 @@
+import React, {useState, createContext, useContext} from 'react';
+import {View} from 'react-native';
+
+import App_stack from './navigation/app_stack';
+import Auth_stack from './navigation/auth_stack';
+const MyContext = createContext();
+
+function App() {
+  const [isLogIn, setIsLogIn] = useState(false);
+
+  // Step 2: Create a provider component
+  const MyProvider = ({children}) => {
+    const [count, setCount] = useState(0);
+
+    const incrementCount = () => {
+      setCount(prevCount => prevCount + 1);
+    };
+
+    return (
+      <MyContext.Provider value={{count, incrementCount}}>
+        {children}
+      </MyContext.Provider>
+    );
+  };
+
+  return (
+    <View>
+      {isLogIn ? (
+        <MyProvider>
+          <App_stack />
+        </MyProvider>
+      ) : (
+        <Auth_stack isLogIn={isLogIn} setIsLogIn={setIsLogIn} />
+      )}
+    </View>
+  );
+}
+
+export default App;
+export const useMyContext = () => useContext(MyContext);
