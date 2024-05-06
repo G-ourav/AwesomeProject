ce0765a5d9ecaaf50cc05e35cd592756055cabde
JkInfosysGloble <85567438+G-ourav@users.noreply.github.com>
2024-05-06 18:31:54 +0530

    Update app_stack.jsx

--- navigation/app_stack.jsx
+++ navigation/app_stack.jsx
@@ -1,13 +1,26 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import {NativeRouter, Routes, Route} from 'react-router-native';
+import Leave_attendance from '../screens/leave_attendance';
+import Chat from '../screens/chat';
+import Profile from '../screens/profile';
 import Home from '../screens/home';
+import Layout from '../screens/layout';
 
 const App_stack = () => {
   return (
     <NativeRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route element={<Layout />}>
+          <Route exact path="/" element={<Home />} />
+          <Route
+            exact
+            path="/leave_attendance"
+            element={<Leave_attendance />}
+          />
+          <Route exact path="/chat" element={<Chat />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </NativeRouter>
   );
