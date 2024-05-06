6d0b1de43812c11c68acb3f6959c426e2273476a
JkInfosysGloble <85567438+G-ourav@users.noreply.github.com>
2024-05-06 18:32:02 +0530

    Create leave_attendance.jsx

---
+++ screens/leave_attendance.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Leave_attendance = () => {
+  return (
+    <View>
+      <Text>Leave_attendance</Text>
+    </View>
+  );
+};
+
+export default Leave_attendance;
