5079f1e92b74b58179403adeb13ab51ea3bd0c82
JkInfosysGloble <85567438+G-ourav@users.noreply.github.com>
2024-05-07 18:58:45 +0530

    Create employee_detail_line.jsx

---
+++ components/employee_detail_line.jsx
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Employee_detail_line = () => {
+  return (
+    <View>
+      <View>
+        <Text>Employee_detail_line</Text>
+      </View>
+      <View>
+        <Text>asdf</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Employee_detail_line;
