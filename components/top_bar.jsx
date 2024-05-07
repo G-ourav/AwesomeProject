73a5ce873bb162804fffb422533905f1b3a3148f
JkInfosysGloble <85567438+G-ourav@users.noreply.github.com>
2024-05-07 18:58:55 +0530

    Create top_bar.jsx

---
+++ components/top_bar.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Top_bar = () => {
+  return (
+    <View>
+      <Text>Top_bar</Text>
+    </View>
+  );
+};
+
+export default Top_bar;
