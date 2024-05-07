b96cc0701d51de8b7457b9e271596082883fb4d7
JkInfosysGloble <85567438+G-ourav@users.noreply.github.com>
2024-05-07 18:59:05 +0530

    Update layout.jsx

--- screens/layout.jsx
+++ screens/layout.jsx
@@ -1,16 +1,34 @@
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, Dimensions, StyleSheet} from 'react-native';
 import {Outlet, Link} from 'react-router-native';
 import React from 'react';
 import Bottum_bar from '../components/bottum_bar';
 
 const Layout = () => {
+  const screenHeight = Dimensions.get('window').height;
   return (
-    <View>
-      <Outlet />
-
-      <Bottum_bar />
-    </View>
+    <ScrollView>
+      <View style={[Styles.container, {height: screenHeight * 0.98}]}>
+        <Outlet />
+        <View style={Styles.bottumBar}>
+          <Bottum_bar />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+const Styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#141414',
+  },
+
+  bottumBar: {
+    position: 'absolute',
+    zIndex: 1000,
+    flexDirection: 'row',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+});
 
 export default Layout;
