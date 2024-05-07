7f57b4ac508983e470e5be20d7b9289b8529bbd7
JkInfosysGloble <85567438+G-ourav@users.noreply.github.com>
2024-05-07 18:59:02 +0530

    Update home.jsx

--- screens/home.jsx
+++ screens/home.jsx
@@ -7,7 +7,7 @@ import {
   ScrollView,
   useColorScheme,
 } from 'react-native';
-import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import Slider_card from '../components/Slider_card';
 import {useMyContext} from '../App';
 
@@ -18,26 +18,27 @@ const Home = () => {
 
   return (
     <ScrollView>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView>
         <View style={styles.TopBox}>
           <View style={styles.TextBox}>
-            <Text
+            {/* <Text
               style={[
                 styles.Text_css,
                 isDarkMode ? styles.whiteText : styles.darkText,
               ]}>
               {count || 'Name of song'}
-            </Text>
+            </Text> */}
+            <Text style={styles.Text_css}>JK Payroll</Text>
           </View>
           <View onTouchStart={incrementCount}>
-            <Entypo
-              name="chevron-right"
-              size={30}
-              color={isDarkMode ? styles.whiteText : styles.darkText}
-            />
+            <AntDesign name="bars" size={30} color="#ffda85" />
           </View>
         </View>
-        <Slider_card />
+        <View style={styles.container}>
+          <View>
+            <Slider_card />
+          </View>
+        </View>
       </SafeAreaView>
     </ScrollView>
   );
@@ -46,9 +47,7 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 23,
     paddingHorizontal: 5,
-    paddingVertical: 24,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -60,20 +59,23 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     height: '100%',
+    padding: 10,
     justifyContent: 'space-between',
     alignItems: 'center',
     alignContent: 'space-evenly',
+    backgroundColor: '#292929',
     flexDirection: 'row',
   },
 
   Text_css: {
     fontSize: 23,
+    color: '#ffda85',
     marginHorizontal: 'auto',
     fontStyle: 'normal',
     borderRadius: 10,
     justifyContent: 'flex-start',
     padding: 5,
-    backgroundColor: '#568f8f',
+    backgroundColor: '#525252',
     fontWeight: 'bold',
   },
 
