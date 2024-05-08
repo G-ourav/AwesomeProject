acef291c999ed36a94f4e5d7b8fb3a02de47c272
JkInfosysGloble <85567438+G-ourav@users.noreply.github.com>
2024-05-08 19:06:10 +0530

    Update bottum_bar.jsx

--- components/bottum_bar.jsx
+++ components/bottum_bar.jsx
@@ -3,7 +3,8 @@ import {Outlet, Link} from 'react-router-native';
 import Feather from 'react-native-vector-icons/Feather';
 import Entypo from 'react-native-vector-icons/Entypo';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-// format-list-checks
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import React from 'react';
 
@@ -13,30 +14,41 @@ const Bottum_bar = () => {
       <View style={Styles.container}>
         <Link to="/">
           <View style={Styles.iconView}>
-            <Entypo name="home" size={30} color="#ffda85" />
+            <Entypo name="home" size={20} color="#ffda85" />
             <Text style={Styles.iconText}>Home</Text>
           </View>
         </Link>
         <Link to="/chat">
           <View style={Styles.iconView}>
-            <Entypo name="chat" size={30} color="#ffda85" />
+            <Entypo name="chat" size={20} color="#ffda85" />
             <Text style={Styles.iconText}>Chat</Text>
           </View>
         </Link>
-        <Link to="/profile">
-          <View style={Styles.iconView}>
-            <MaterialCommunityIcons name="face-man" size={30} color="#ffda85" />
-            <Text style={Styles.iconText}>Profile</Text>
-          </View>
-        </Link>
+
         <Link to="/leave_attendance">
           <View style={Styles.iconView}>
             <MaterialCommunityIcons
               name="format-list-checks"
-              size={30}
+              size={20}
               color="#ffda85"
             />
             <Text style={Styles.iconText}>Attendance</Text>
+          </View>
+        </Link>
+        <Link to="/leave_attendance">
+          <View style={Styles.iconView}>
+            <FontAwesome name="search-plus" size={20} color="#ffda85" />
+            <Text style={Styles.iconText}>Leads</Text>
+          </View>
+        </Link>
+        <Link to="/profile">
+          <View style={Styles.iconView}>
+            <MaterialIcons
+              name="precision-manufacturing"
+              size={20}
+              color="#ffda85"
+            />
+            <Text style={Styles.iconText}>Products</Text>
           </View>
         </Link>
       </View>
@@ -54,10 +66,11 @@ const Styles = StyleSheet.create({
     flex: 1,
     alignContent: 'center',
     alignItems: 'center',
-    padding: 10,
-    width: 100,
+    padding: 2,
+    width: 78,
   },
   iconText: {
+    fontSize: 10,
     color: '#ffda85',
   },
 });
